04e3c18d1a5acc03126511f4b0fb51af194d40e2
Mi shok <mishaonukrnet@ukr.net>
2022-12-23 16:02:54 +0200

    Add app design

---
+++ src/InvoiceController.java
@@ -0,0 +1,8 @@
+public class InvoiceController{
+    //Scheduled pay
+    public void autoPay(String request){
+        //Convert request to the InvoiceModel object
+    }
+
+    public void
+}
---
+++ src/InvoiceModel.java
@@ -0,0 +1,9 @@
+public class InvoiceModel{
+    String rate;
+    int payYear;
+    int payMonth;
+    //Auto or manual
+    String payType;
+    //null if type is not auto
+    String cardData;
+}
